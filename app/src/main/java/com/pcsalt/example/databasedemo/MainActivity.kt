379ae22a037d56bb8a1ae4582c71cdb8c700af85
krrishnaaaa <krishna@pcsalt.com>
2020-04-22 09:54:37 +0530

    included update/delete/select

--- app/src/main/java/com/pcsalt/example/databasedemo/MainActivity.kt
+++ app/src/main/java/com/pcsalt/example/databasedemo/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
+
 class MainActivity : AppCompatActivity() {
     private var myDatabase: MyDatabase? = null
     private val TAG = "MainActivity"
@@ -19,13 +20,31 @@ class MainActivity : AppCompatActivity() {
         myDatabase = MyDatabase(this)
         myDatabase?.open()
 
-        btn_save.setOnClickListener {
+        btn_insert.setOnClickListener {
             if (isValidInfo()) {
-                saveInfo()
+                insert()
+                clearFields()
             } else {
                 Toast.makeText(this@MainActivity, "Enter all details", Toast.LENGTH_SHORT).show()
             }
         }
+        btn_update.setOnClickListener{
+            if (isValidInfo()) {
+                update()
+                clearFields()
+            } else {
+                Toast.makeText(this@MainActivity, "Enter all details", Toast.LENGTH_SHORT).show()
+            }
+        }
+        btn_delete.setOnClickListener{
+            if (isValidInfo()) {
+                delete()
+                clearFields()
+            } else {
+                Toast.makeText(this@MainActivity, "Enter all details", Toast.LENGTH_SHORT).show()
+            }
+        }
+        btn_select.setOnClickListener{ select() }
     }
 
     override fun onDestroy() {
@@ -34,11 +53,20 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
     }
 
+    private fun getName() = et_name.text.toString()
+
+    private fun getContact() = et_contact.text.toString()
+
     private fun isValidInfo(): Boolean {
-        return (et_name.text.toString().isNotEmpty()) && (et_contact.text.toString().isNotEmpty())
+        return (getName().isNotEmpty()) && (getContact().isNotEmpty())
     }
 
-    private fun saveInfo() {
+    private fun clearFields() {
+        et_name.setText("")
+        et_contact.setText("")
+    }
+
+    private fun insert() {
         val contentValues = ContentValues()
         contentValues.put(DBConsts.NAME, et_name.text.toString())
         contentValues.put(DBConsts.CONTACT, et_contact.text.toString())
@@ -47,5 +75,52 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "inserted id: $id")
     }
 
+    private fun update() {
+        val whereClause: String = DBConsts.NAME + "=?"
+        val whereArgs = arrayOf(getName())
+
+        val contentValues = ContentValues()
+        contentValues.put(DBConsts.CONTACT, getContact())
+
+        val updatedRows = myDatabase?.update(DBConsts.TBL_CONTACT, contentValues, whereClause, whereArgs)
+        Log.d(TAG, "updated rows $updatedRows")
+    }
+
+    private fun delete() {
+        val whereClause: String = DBConsts.NAME+ "=?" + DBConsts.AND + DBConsts.CONTACT + "=?"
+        val whereArgs = arrayOf(
+            getName(),
+            getContact()
+        )
+        val deletedRows = myDatabase?.delete(DBConsts.TBL_CONTACT, whereClause, whereArgs)
+        Log.d(TAG,"Number of deleted rows: $deletedRows")
+    }
+
+    private fun select() {
+        val result = StringBuilder()
+        var id: Int
+        var name: String
+        var contact: String
+        val columns = arrayOf(
+            DBConsts.ID,
+            DBConsts.NAME,
+            DBConsts.CONTACT
+        )
+        val selection: String? = null // no selection
+        val selectionArgs: Array<String>? = null // not needed
+        val cur = myDatabase?.select(DBConsts.TBL_CONTACT, columns, selection, selectionArgs)
+        //check if cursor returned from select statement is not null
+        if (cur != null) {
+            while (cur.moveToNext()) {
+                id = cur.getInt(cur.getColumnIndex(DBConsts.ID))
+                name = cur.getString(cur.getColumnIndex(DBConsts.NAME))
+                contact = cur.getString(cur.getColumnIndex(DBConsts.CONTACT))
+                result.append("[id: $id]\t[name: $name]\t[contact: $contact]\n")
+            }
+        } else {
+            result.append("No result found")
+        }
+        tv_content.text = result
+    }
 
 }
